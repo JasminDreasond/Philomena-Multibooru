71c803f0995fa4a8e56b230ee988a8869a13f34b
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2026-03-07 22:46:38 -0300

    tags file migration

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap import
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
+import './tags.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
