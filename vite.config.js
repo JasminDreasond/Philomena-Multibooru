5c5f656f55482d00288f666d8313c8e8639809e2
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2026-03-17 10:25:40 -0300

    copyIndexTo404 added into builder.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Resolving __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * Custom plugin to copy index.html to 404.html for GitHub Pages compatibility.
+ * @returns {import('vite').Plugin}
+ */
+const copyIndexTo404 = () => {
+  return {
+    name: 'copy-index-to-404',
+    /** @type {function} */
+    closeBundle: () => {
+      const distPath = path.resolve(__dirname, 'dist');
+      const indexPath = path.resolve(distPath, 'index.html');
+      const targetPath = path.resolve(distPath, '404.html');
+
+      if (fs.existsSync(indexPath)) {
+        fs.copyFileSync(indexPath, targetPath);
+      }
+    },
+  };
+};
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +34,5 @@ export default defineConfig({
   build: {
     assetsDir: 'assets',
   },
-  plugins: [react()],
+  plugins: [react(), copyIndexTo404()],
 });
